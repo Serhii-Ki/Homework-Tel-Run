126aad327536966c1c03f096f74429730ef4544b
Serhii-Ki <123417671+Serhii-Ki@users.noreply.github.com>
2023-02-10 00:38:40 +0300

    Add files via upload

---
+++ java_homework_2/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание №1
+        int num1 = 89;
+        byte num2 = 4;
+        short num3 = 56;
+        float num4 = 4.7333436f;
+        double num5 = 4.355453532;
+        long num6 = 12121;
+        char c = 'G';
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(c);
+        // Задание №2
+        int num = 321;
+        int number3 = num % 10;
+        int number2 = num / 10 % 10;
+        int number1 = num / 100;
+        System.out.println(number3);
+        System.out.println(number2);
+        System.out.println(number1);
+    }
+}
+
