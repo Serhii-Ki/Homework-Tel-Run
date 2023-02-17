86e46bd309d8c2b1239c90fb901e548c8dc9d98e
Serhii-Ki <123417671+Serhii-Ki@users.noreply.github.com>
2023-02-17 21:09:53 +0300

    Add files via upload

---
+++ java_homework/src/Game/Constant.java
@@ -0,0 +1,9 @@
+package Game;
+
+public class Constant {
+    static int power;
+
+    static int charisma;
+
+    static int magic;
+}
---
+++ java_homework/src/Study/Main.java
@@ -0,0 +1,12 @@
+package Study;
+
+public class Main {
+    public static void main(String[] args) {
+
+        home_pat2 student = new home_pat2();
+
+        student.getStr();
+
+        student.getStr2();
+    }
+}
